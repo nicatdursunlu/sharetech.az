dd6f9702f200bb24b142c6c25ffd35758c931db8
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2021-04-04 11:13:08 +0400

    advertising

---
+++ src/components/Advertising/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="advertising">
+      <h2 class="advertising__title">Reklam ucun</h2>
+      <p class="advertising__desc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing edit
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "./style.scss";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <HeaderNews />
   <div class="divider" />
   <MainNews />
+  <Advertising />
 </template>
 
 <script>
 import HeaderNews from "../components/HeaderNews";
 import MainNews from "../components/MainNews";
+import Advertising from "../components/Advertising";
 
 export default {
   name: "Home",
   components: {
     HeaderNews,
     MainNews,
+    Advertising,
   },
 };
 </script>
